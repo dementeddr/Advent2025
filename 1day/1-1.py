f4dddaaa35d7174fd44114ed8374bf5560678f83
dementeddr <2413901+dementeddr@users.noreply.github.com>
2025-12-01 15:35:22 -0500

    Day 1 done. It's like swerve wheels but not

---
+++ 1day/1-1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import sys
+import re
+from time import perf_counter_ns
+
+_day = 1 #Advent of Code day
+
+## BEGIN SOLUTION
+
+def main(data):
+
+    point = 50
+    count = 0
+
+    for line in data:
+        turn = line[0]
+        dist = int(line[1:])
+
+        if turn == 'L':
+            dist = -dist
+
+        point = (point + dist) % 100
+
+        if point == 0:
+            count += 1
+            
+        print(f"{line[:-1]} -> {dist} = {point}, c {count}")
+
+    print(f"Zero-point count = {count}")
+
+    
+## END SOLUTION
+
+if __name__ == "__main__":
+    input_file = f"input-d{_day}.txt"
+    data = []
+
+    if len(sys.argv) > 1:
+        input_file = sys.argv[1]
+
+    with open(input_file, "r") as fp:
+        data = fp.readlines()
+
+    time_start = perf_counter_ns()
+    main(data)
+    time_stop = perf_counter_ns()
+
+    time = (time_stop - time_start) / 1000000
+    print(f"\nExecution took {time:.4f} milliseconds")
+
