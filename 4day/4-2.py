2dcef9583a67f9f993d7bcf0c9a71d0fb4f39bd4
dementeddr <2413901+dementeddr@users.noreply.github.com>
2025-12-04 23:45:14 -0500

    Day 4 done. OPTIMIZING FORKLIFT TASKS IS MY DAY JOB DAMMIT

---
+++ 4day/4-2.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+import sys
+import re
+from time import perf_counter_ns
+
+_day = 4 #Advent of Code day
+
+## BEGIN SOLUTION
+
+def main(data):
+
+    grid = list(map(list, data))
+    total = 0
+    rolls_removed = True
+
+    while rolls_removed:
+        rolls_removed = False
+        for y in range(len(grid)):
+            for x in range(len(grid[y])):
+                if grid[y][x] == '@' and count_adj(grid, x, y) < 4:
+                    grid[y][x] = 'x'
+                    total += 1
+                    rolls_removed = True
+
+    list(map(lambda l: print(''.join(l)), grid))
+    print(f"\nFree rolls: {total}")
+
+
+def count_adj(grid, x, y):
+    count = 0
+    for dy in range(-1,2): 
+        for dx in range(-1, 2):
+            if dx == 0 and dy == 0:
+                continue
+            if is_in_grid(grid, x+dx, y+dy) and grid[y+dy][x+dx] == '@':
+                count += 1
+    return count
+
+
+def is_in_grid(grid, x, y):
+    return 0 <= y < len(grid) and 0 <= x < len(grid[y])
+
+    
+## END SOLUTION
+
+if __name__ == "__main__":
+    input_file = f"input-d{_day}.txt"
+    lines = []
+
+    if len(sys.argv) > 1:
+        input_file = sys.argv[1]
+
+    with open(input_file, "r", encoding="ascii") as fp:
+        lines = list(map(lambda l: l[:-1], fp.readlines()))
+
+    time_start = perf_counter_ns()
+    main(lines)
+    time_stop = perf_counter_ns()
+
+    time = (time_stop - time_start) / 1000000
+    print(f"\nExecution took {time:.4f} milliseconds")
+
