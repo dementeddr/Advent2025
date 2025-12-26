2e00ec73a817403b0f5011069b03a91c476b6674
dementeddr <2413901+dementeddr@users.noreply.github.com>
2025-12-25 20:59:57 -0500

    7.1 was pretty easy. He said, forshadowingly

---
+++ 7day/7-1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import sys
+import re
+from time import perf_counter_ns
+
+_day = 7 #Advent of Code day
+
+## BEGIN SOLUTION
+
+def main(data):
+
+    beams = [False for _ in range(len(data[0]))]
+    start = data[0].find("S")
+    beams[start] = True
+    count = 0
+
+    for line in data[1:]:
+        new_beams = beams.copy()
+        
+        for i, x in enumerate(line):
+            if x == '^' and new_beams[i]:
+                count += 1
+                new_beams[i] = False
+                new_beams[i-1] = True
+                new_beams[i+1] = True 
+
+        beams = new_beams
+
+    print(f"Total beams split = {count}")
+
+
+
+    
+## END SOLUTION
+
+if __name__ == "__main__":
+    input_file = f"input-d{_day}.txt"
+    lines = []
+
+    if len(sys.argv) > 1:
+        input_file = sys.argv[1]
+
+    with open(input_file, "r", encoding="ascii") as fp:
+        lines = list(map(lambda l: l[:-1], fp.readlines()))
+
+    time_start = perf_counter_ns()
+    main(lines)
+    time_stop = perf_counter_ns()
+
+    time = (time_stop - time_start) / 1000000
+    print(f"\nExecution took {time:.4f} milliseconds")
+
